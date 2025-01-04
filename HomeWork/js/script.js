0c669f26c413b6ed8566b06d548fe1738ce58ece
MaxFell <67173616+MaxFell310@users.noreply.github.com>
2025-01-04 14:05:00 +0000

    Home task

--- HomeWork/js/script.js
+++ HomeWork/js/script.js
@@ -8,12 +8,26 @@
 //   { id: 2, name: "Cherry" },
 //   { id: 3, name: "Orange" },
 // ];
+// let result = fruts.map((item) => item.name);
+// let newString = result.join(", ")
+// console.log(newString);
 
 //  -- 2 --
 //Виведіть парні числа від 2 до 10, використовуючи цикл for.
+// for (let i = 1; i < 10; i++){
+//     if(i % 2 !== 0){
+//         continue
+//     }
+//     console.log(i)
+// }
 
 //  -- 3 --
 //Замініть цикл "for" на "while"
+// let a = 0
+// while(a < 5){
+//     a++;
+//     console.log(`цифра ${a}!`)
+// }
 // for (let i = 0; i < 5; i++) {
 //     console.lpg( `цифра ${i}!` );
 //   }
@@ -23,6 +37,21 @@
 //Якщо відвідувач введе менше число – попросити ввести ще раз, і так далі.
 //Цикл повинен запитувати число доти, доки відвідувач не введе число,
 // більше за 100, або не скасує ввід/введе порожній рядок.
+// let number;
+// while (true) {
+//   number = prompt("Введіть число більше за 100:");
+//   if (number === null || number === "") {
+//     alert("Ввід скасовано або порожній рядок.");
+//     break;
+//   }
+//   number = Number(number);
+//   if (number > 100) {
+//     alert("Число прийнято!");
+//     break;
+//   } else {
+//     alert("Число повинно бути більше за 100. Спробуйте ще раз.");
+//   }
+// }
 
 //  -- 5 --
 // Вирахуйте середній вік
@@ -33,3 +62,5 @@
 //   { age: 10, name: "Юля" },
 //   { age: 20, name: "Катя" },
 // ];
+//   const avgAge = girls.reduce((sum, girls) => sum + girls.age,0)/girls.length
+//   console.log(avgAge)
